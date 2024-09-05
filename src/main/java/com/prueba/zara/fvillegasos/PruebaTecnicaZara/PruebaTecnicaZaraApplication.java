d11e5e6fd7484970f6ae78f1ae0a9011b20ca5c6
fvillegasos <154078493+fvillegasos@users.noreply.github.com>
2024-09-05 18:24:48 +0200

    master: first code generation

---
+++ src/main/java/com/prueba/zara/fvillegasos/PruebaTecnicaZara/PruebaTecnicaZaraApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.zara.fvillegasos.PruebaTecnicaZara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaZaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaZaraApplication.class, args);
+	}
+
+}
